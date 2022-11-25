9fbd17c00b879fb1e69c1d1e96c9e7660743a74a
Haldadinisilma <90280076+Haldadinisilma@users.noreply.github.com>
2022-11-25 14:06:02 +0700

    Add files via upload

---
+++ storage/framework/views/829c7348e022ad96b092deeb780f87199d25ba6c.php
@@ -0,0 +1,68 @@
+
+
+<?php $__env->startSection('content'); ?>
+<div class="card mt-5">
+        <div class="card-header">
+            <h2>CRUD</h2>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-12 mt-1 mr-1">
+                    <div class="float-right">
+                        <a class="btn btn-primary" href="<?php echo e(route('posts.index')); ?>"> 
+                          Back</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col-lg-12">
+                    <?php if($message = Session::get('success')): ?>
+                        <div class="alert alert-success">
+                            <p><?php echo e($message); ?></p>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <div class="col-lg-12">
+                    <?php if($errors->any()): ?>
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> Data Tidak Boleh Kosong.<br><br>
+                            <ul>
+                                <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <li><?php echo e($error); ?></li>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            </ul>
+                        </div>
+                    <?php endif; ?>
+
+                    <form action="<?php echo e(route('posts.store')); ?>" method="POST">
+                        <?php echo csrf_field(); ?>
+
+                         <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Title:</strong>
+                                    <input type="text" name="title" class="form-control" 
+                                      placeholder="Title">
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Description:</strong>
+                                    <textarea class="form-control" rows="6" 
+                                      name="description" placeholder="Description"> 
+                                     </textarea>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                <button type="submit" class="btn btn- 
+                                   success">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('posts.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\USER\projectlaravel\resources\views/posts/create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/ce58af92f8c6b345ec60f46286cfa287c820ea2a.php
@@ -0,0 +1,48 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="card mt-5">
+        <div class="card-header">
+            <h2>Laravel 8 CRUD</h2>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-12 mt-1 mr-1">
+                    <div class="float-right">
+                        <a class="btn btn-primary" href="<?php echo e(route('posts.index')); ?>"> 
+                          Back</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col-lg-12">
+                    <?php if($message = Session::get('success')): ?>
+                        <div class="alert alert-success">
+                            <p><?php echo e($message); ?></p>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+                            <div class="form-group">
+                                <strong>Title:</strong>
+                                <?php echo e($post->title); ?>
+
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12">
+                            <div class="form-group">
+                                <strong>Description:</strong>
+                                <?php echo e($post->description); ?>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('posts.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\USER\projectlaravel\resources\views/posts/show.blade.php ENDPATH**/ ?>
